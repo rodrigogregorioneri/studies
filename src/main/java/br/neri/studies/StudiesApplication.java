501af6ea0cc423561faacbc0e6ad459c7e170c8d
T803927 <T803927@TI0034412.tokiomarine.com.br>
2018-08-20 15:04:59 -0300

    Subindo Starter da aplicação

---
+++ src/main/java/br/neri/studies/StudiesApplication.java
@@ -0,0 +1,12 @@
+package br.neri.studies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiesApplication.class, args);
+	}
+}
