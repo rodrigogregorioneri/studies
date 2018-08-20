628f7a8064f038b010d35df08769586855f2029e
T803927 <T803927@TI0034412.tokiomarine.com.br>
2018-08-20 16:47:34 -0300

    Melhorando Cors utilizando CorsFilter

--- src/main/java/br/neri/studies/config/CorsConfigure.java
+++ src/main/java/br/neri/studies/config/CorsConfigure.java
@@ -1,17 +1,29 @@
 package br.neri.studies.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfigure implements WebMvcConfigurer {
+public class CorsConfigure {
 
-	@Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+	@Bean
+    public CorsFilter  corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+
     }
 	
 }
